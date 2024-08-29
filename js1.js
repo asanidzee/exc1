1356c445827a845203ab846eb235b9bf2f8fde3b
asanidzee <131695143+asanidzee@users.noreply.github.com>
2024-08-29 03:32:26 -0700

    js1

---
+++ js1.js
@@ -0,0 +1,25 @@
+// exercise 1
+let num = 100;
+
+if (num < 50) {
+    console.log("ნაკლებია 50-ზე");
+} else if (num > 20){
+    console.log("მეტია 20-ზე")
+} else {
+    console.log("შეცდომა!")
+}
+
+// exercise 2
+let namee = "მარიამ";
+
+let mariam = (age) ? "true" : "false";
+console.log(mariam)
+
+// exercise 3
+    let m = მარიამ
+    function greet(namee){
+        switch(x){
+            case(m):
+                return true;
+            }
+        }
